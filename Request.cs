56c63c4a7163a23f34689827323d65a1d17c781c
ThisAintExo <122402395+ThisAintExo@users.noreply.github.com>
2023-01-10 15:20:08 -0500

    Create Request.cs

---
+++ Request.cs
@@ -0,0 +1,35 @@
+using RestSharp;
+using System;
+using System.Text.Json;
+
+namespace Wow
+{
+    public class used
+    {
+        public class DogJson
+        {
+            public string message { get; set; }
+            public string status { get; set; }
+        }
+        public static void Main(string[] args)
+        {
+            static void DogReq(String url)
+            {
+                var client = new RestClient(url);
+                var request = new RestRequest(url);
+                var response = client.Get(request);
+
+                var response_decoded = JsonSerializer.Deserialize<DogJson>(response.Content);
+
+                Console.WriteLine($"{response_decoded.message} - {response_decoded.status}");
+            }
+
+            while (true)
+            {
+                DogReq("https://dog.ceo/api/breeds/image/random");
+                DogReq("https://dog.ceo/api/breeds/image/random");
+                DogReq("https://dog.ceo/api/breeds/image/random");
+            }
+        }
+    }
+}
